cbf5e777bf05ef7de0106e405b5ee9facbe5becb
Anaïs L'Haridon <aclavellharidon@gmail.com>
2022-02-23 12:09:04 +0100

    ajout Boutique dans le test

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,5 +1,6 @@
 package test;
 
+import metier.Boutique;
 import metier.Console;
 import metier.Jeu;
 
@@ -9,11 +10,13 @@ public class Test {
 		Console ps = new Console("ps5");
 		Console xbox = new Console("xbox");
 		
-		Jeu jeu1 = new Jeu("God of war", ps);
-		Jeu jeu2 = new Jeu("PES 2022", ps);
-		Jeu jeu3 = new Jeu("FIFA 2022", xbox);
-		Jeu jeu4 = new Jeu("GTA V", ps);
-		Jeu jeu5 = new Jeu("PUBG", xbox);
+		Boutique boutique = new Boutique("Test","");
+		
+		Jeu jeu1 = new Jeu("God of war", ps, boutique);
+		Jeu jeu2 = new Jeu("PES 2022", ps, boutique);
+		Jeu jeu3 = new Jeu("FIFA 2022", xbox, boutique);
+		Jeu jeu4 = new Jeu("GTA V", ps, boutique);
+		Jeu jeu5 = new Jeu("PUBG", xbox, boutique);
 
 
 
